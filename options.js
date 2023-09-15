a9d044adcad3c83b0357d423bce4b499d76e3251
Airplane <letadllo@gmail.com>
2023-09-15 11:17:51 +0500

    Implemented object groups?

--- options.js
+++ options.js
@@ -1,14 +1,49 @@
 import "./libs/tabulator/js/tabulator.min.js"
 import {createSearchObjectsTable} from "./scripts/search-objects-table.js"
 
-var searchObjectsTable;
+var searchObjectsTable = undefined;
+var settingsJSON = undefined;
+const defaultSettings = JSON.parse(`{
+    "currentObjectGroup": 0,
+    "searchObjectGroups": [
+        [
+            {
+            "aliases": ["綾小路清隆", "綾小路", "清隆"],
+            "color": "#AFE1AF",
+            "description": "character 0"
+            },
+            {
+            "aliases": ["堀北鈴音", "堀北", "鈴音"],
+            "color": "#FF5733",
+            "description": "character 1"
+            },
+            {
+            "aliases": ["櫛田桔梗", "櫛田", "桔梗"],
+            "color": "#00FFFF",
+            "description": "character 2"
+            }
+        ],
+        [
+            {
+            "aliases": ["須藤健", "須藤", "健"],
+            "color": "#DFFF00",
+            "description": "character 3"
+            },
+            {
+            "aliases": ["軽井沢恵", "軽井沢", "恵"],
+            "color": "#DF00FF",
+            "description": "character 4"
+            }
+        ]
+    ]
+}`);
 
-const saveTextOptions = () => 
+function saveTextOptions ()
 {
-    const searchJSON = document.getElementById("searchJSON").value;
+    settingsJSON = JSON.parse(document.getElementById("settingsJSON").value);
   
     chrome.storage.sync.set(
-      { searchJSON: searchJSON },
+      { settingsJSON: settingsJSON },
       () => {
         const status = document.getElementById('status');
         status.textContent = 'Options saved.';
@@ -16,39 +51,64 @@ const saveTextOptions = () =>
           status.textContent = '';
         }, 750);
 
-        searchObjectsTable.setData(searchJSON)
+        loadTable();
       }
     );
-};
+}
 
-const saveTableOptions = () => 
+function saveTableOptions () //redo
 {
-  const searchJSON = JSON.stringify(searchObjectsTable.getData(), null, 2);
-  chrome.storage.sync.set(
-    { searchJSON: searchJSON },
-    () => {
-      const status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(() => {
-        status.textContent = '';
-      }, 750);
+    //const settingsJSON = JSON.stringify(searchObjectsTable.getData(), null, 2);
+    settingsJSON.searchObjectGroups[settingsJSON.currentObjectGroup] = searchObjectsTable.getData;
+    chrome.storage.sync.set(
+        { settingsJSON: settingsJSON },
+        () => {
+            const status = document.getElementById('status');
+            status.textContent = 'Options saved.';
+            setTimeout(() => {
+            status.textContent = '';
+            }, 750);
 
-      searchObjectsTable.setData(searchJSON)
-    }
-  );
+            loadTable();
+        }
+    );
 
-  document.getElementById("searchJSON").value = searchJSON;
-};
+    document.getElementById("settingsJSON").value = JSON.stringify(settingsJSON, null, 2);
+}
   
-const restoreOptions = () => 
+function restoreOptions ()
 {
-  chrome.storage.sync.get("searchJSON").then((result) =>
-  {
-    searchObjectsTable = createSearchObjectsTable(result.searchJSON, "#search-objects-table")
-    document.getElementById('searchJSON').value = result.searchJSON;
-  });
-};
+    chrome.storage.sync.get("settingsJSON").then((result) =>
+    {
+        try
+        {
+            settingsJSON = result.settingsJSON;
+            loadTable();
+            document.getElementById('settingsJSON').value = JSON.stringify(settingsJSON, null, 2);
+        }
+        catch(e)
+        {
+            console.log('invalid JSON:');
+            console.log(e);   
+        }
+    });
+}
+
+function loadTable()
+{
+    console.log(settingsJSON);
+    searchObjectsTable = createSearchObjectsTable(settingsJSON.searchObjectGroups[settingsJSON.currentObjectGroup], "#search-objects-table");
+}
+
+function resetStorage()
+{
+    chrome.storage.sync.clear();
+    chrome.storage.sync.set({settingsJSON: defaultSettings});
+    window.location.reload();
+}
+
   
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('save-text').addEventListener('click', saveTextOptions);
 document.getElementById('save-table').addEventListener('click', saveTableOptions);
+document.getElementById('reset').addEventListener('click', resetStorage);
