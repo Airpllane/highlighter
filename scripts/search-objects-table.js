bf6ce4c74e0b2a181f8215f191662dfe68e464b4
Androix777 <androix77@gmail.com>
2023-10-16 02:29:41 +0500

    Aliases modal editor

--- scripts/search-objects-table.js
+++ scripts/search-objects-table.js
@@ -1,137 +1,165 @@
 export function createSearchObjectsTable(tableData, divID)
 {
-
-    var rowMenu = 
-    [
+    var table = new Tabulator(divID,
         {
-            label:"Add Row",
-            action:function(e, row)
-            {
-                row.getTable().addData([{aliases:["New"], color:'#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'), description:"New description"}], false);
-            }
-        },
-        {
-            label:"Delete Row",
-            action:function(e, row)
-            {
-                row.delete();
-            }
-        }
-    ];
-
-    var table = new Tabulator(divID, 
-    {
-        layout:"fitColumns",
-        data:tableData,
-        height: 500,
-        rowContextMenu: rowMenu,
-        movableColumns:true,
-        movableRows: true,
-        columns:
-        [
-            {
-                title:"Aliases", 
-                field:"aliases", 
-                editor: function tagStringEditor(cell, onRendered, success, cancel, editorParams)
+            layout: "fitColumns",
+            data: tableData,
+            height: 500,
+            movableColumns: true,
+            movableRows: true,
+            rowContextMenu: [
                 {
-                    var cellValue = cell.getValue().toString();
-                    var input = document.createElement("input");
-                
-                    input.value = cellValue;
-                
-                    onRendered
-                    (
-                        function()
-                        {
-                            input.focus();
-                            input.style.height = "100%";
-                        }
-                    );
-                
-                    function onChange()
+                    label: "Add item",
+                    action: function (e, row)
                     {
-                        if(input.value != cellValue)
+                        row.getTable().addData([{ aliases: ["New"], color: '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'), description: "New description" }], false);
+                    }
+                },
+                {
+                    label: "Delete item",
+                    action: function (e, row)
+                    {
+                        row.delete();
+                    }
+                }
+            ],
+            columns:
+                [
+                    {
+                        title: "Aliases",
+                        field: "aliases",
+                        editor: aliasesEditor
+                    },
+                    {
+                        title: "Description",
+                        field: "description",
+                        editor: "input"
+                    },
+                    {
+                        title: "Color",
+                        field: "color",
+                        formatter: 'color',
+                        editor: function (cell, onRendered, success, cancel)
                         {
-                            console.log(input.value.split(","))
-                            success(input.value.split(","));
-                        }
-                        else
-                        {
-                            cancel();
+                            var cellValue = cell.getValue();
+                            var input = document.createElement("input");
+
+                            input.setAttribute("type", "color");
+                            input.setAttribute("value", cellValue);
+
+                            input.style.height = "100%";
+                            input.style.width = "100%";
+
+                            onRendered(() =>
+                            {
+                                input.focus();
+                            });
+
+                            function onChange()
+                            {
+                                console.log(input.value)
+                                if (input.value != cellValue)
+                                {
+                                    success(input.value);
+                                }
+                                else
+                                {
+                                    cancel();
+                                }
+                            }
+                            input.addEventListener("change", onChange);
+                            input.addEventListener("keydown", (e) =>
+                            {
+                                if (e.keyCode == 13)
+                                {
+                                    onChange();
+                                }
+
+                                if (e.keyCode == 27)
+                                {
+                                    cancel();
+                                }
+                            });
+                            return input;
                         }
                     }
-                
-                    input.addEventListener("blur", onChange);
-                
-                    input.addEventListener("keydown", (event) =>
-                    {
-                        if(event.key == 'Enter')
-                        {
-                            onChange();
-                        }
-                
-                        if(event.key == 'Esc')
-                        {
-                            cancel();
-                        }
-                    });
-                
-                    return input;
+                ],
+        });
+
+    return table;
+}
+
+function aliasesEditor(cell, onRendered, success, cancel)
+{
+    var cellValue = cell.getValue();
+
+    var container = document.createElement("div");
+    container.style.position = "fixed";
+    container.style.top = "0";
+    container.style.left = "0";
+    container.style.width = "100%";
+    container.style.height = "100%";
+    container.style.zIndex = "9998";
+
+    var blocker = document.createElement("div");
+    blocker.style.width = "100%";
+    blocker.style.height = "100%";
+    blocker.style.backgroundColor = "rgba(0,0,0,0.5)";
+    container.appendChild(blocker);
+
+    var modal = document.createElement("div");
+    modal.style.position = "absolute";
+    modal.style.left = "50%";
+    modal.style.top = "50%";
+    modal.style.transform = "translate(-50%, -50%)";
+    modal.style.backgroundColor = "white";
+    modal.style.padding = "10px";
+    modal.style.width = "400px";
+    modal.style.height = "300px";
+    modal.style.overflow = "auto";
+    container.appendChild(modal);
+
+    var aliasTable = new Tabulator(modal, {
+        data: cellValue.map(alias => ({ alias: alias })),
+        layout: "fitColumns",
+        rowContextMenu: [
+            {
+                label: "Add alias",
+                action: function (e, row)
+                {
+                    row.getTable().addData([{ alias: "New" }], false);
                 }
             },
             {
-                title:"Description", 
-                field:"description", 
-                editor:"input"
-            },
-            {
-                title:"Color",
-                field:"color",
-                formatter:'color',
-                editor:function(cell, onRendered, success, cancel)
+                label: "Delete alias",
+                action: function (e, row)
                 {
-                    var cellValue = cell.getValue();
-                    var input = document.createElement("input");
-                
-                    input.setAttribute("type", "color");
-                    input.setAttribute("value", cellValue);
-
-                    input.style.height = "100%";
-                    input.style.width = "100%";
-
-                    onRendered(() =>
-                    {
-                        input.focus();
-                    });
-
-                    function onChange()
-                    {
-                        console.log(input.value)
-                        if(input.value != cellValue)
-                        {
-                            success(input.value);
-                        }
-                        else
-                        {
-                            cancel();
-                        }
-                    }
-                    input.addEventListener("change", onChange);
-                    input.addEventListener("keydown", (e) =>
-                    {
-                        if(e.keyCode == 13){
-                            onChange();
-                        }
-                
-                        if(e.keyCode == 27){
-                            cancel();
-                        }
-                    });
-                    return input;
+                    row.delete();
                 }
             }
         ],
+        columns: [
+            {
+                title: "Alias",
+                field: "alias",
+                editor: "input"
+            }
+        ]
     });
 
-    return table;
+    function closeModal()
+    {
+        document.body.removeChild(container);
+        document.getElementById("page-content").style.filter = "";
+        var data = aliasTable.getData();
+        success(data.map(item => item.alias));
+    }
+
+    blocker.addEventListener("click", closeModal);
+    document.getElementById("page-content").style.filter = "blur(5px)";
+    document.body.appendChild(container);
+    onRendered(() =>
+    {
+        modal.focus();
+    });
 }
